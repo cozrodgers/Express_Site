f3d8cebe108bedff12005e2e156e3e2094a53488
Corey Rodgers <coreyrodgers95@gmail.com>
2019-04-10 00:40:41 +0100

    add routes and jade templating views, add navbar and index page

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
+//require dependencies
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
-const port = 3000;
-//initialize app
 
+//set port name
+const port = 3000;
+
+//initialize app
 const app = express();
+
+//set template engine setting
+app.set('views', path.join(__dirname, 'views')); //read this folder for the views
+app.set('view engine', 'jade'); //set template engine name
+
+//specify middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+
+//specify public static folder (css etc)
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
 app.get('/', function(req, res){
     console.log('hello world');
-    res.send(`<h1>we are running on port ${port}</h1>`);
+    res.render('index');
    
 });
 
+
+
+//launch server
 app.listen(port);
 console.log(`server is running on port ${port}`);

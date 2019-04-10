c2b6318522c88938d517de6acb5f73fcbcc69991
Corey Rodgers <coreyrodgers95@gmail.com>
2019-04-10 01:31:16 +0100

    add node mailer functionality, complete

--- app.js
+++ app.js
@@ -41,6 +41,48 @@ app.get('/contact', function(req, res){
    
 });
 
+app.post('/contact/send', function(req, res){
+    let transporter = nodeMailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: 'coreyrodgers95@gmail.com',
+            pass: "Intervention10254231095!!"
+        }
+    });
+
+    let mailOptions = {
+        from: 'Corey <coreyrodgers95@gmail.com>',
+        to: 'coreyrodgers95@gmail.com',
+        subject: "Website Submission",
+        text: `You have used node mailer to make a post to yourself these are the following details
+                    name:${req.body.name}
+                    email:${req.body.email}
+                    message:${req.body.message}`,
+        html: `<p>You have used node mailer to make a post to yourself these are the following details</p>
+                <ul>
+                <li>${req.body.name}</li>
+                <li>${req.body.email}</li>
+                <li>${req.body.message}</li>
+                
+                </ul>`        
+
+         };
+         
+    transporter.sendMail(mailOptions, function(err, info) {
+
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        }
+        else {
+            console.log('message sent');
+            res.redirect('/');
+        }
+        
+    })
+
+});
+
 
 
 //launch server
